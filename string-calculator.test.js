46e309483767e85059edc155bf201ceafd415625
MattW <mattnwarnock@yahoo.co.uk>
2023-09-04 18:14:48 +0100

    Refactor tests

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -1,36 +1,33 @@
 const StringCalculator = require('./string-calculator');
 
 describe('String Calculator', () => {
+    const stringCalculator = new StringCalculator;
+
     it('Returns zero when passed an empty string', () => {
-        const stringCalculator = new StringCalculator;
         const result = stringCalculator.add('');
 
         expect(result).toBe(0);
     })
 
     it('returns sum of two comma separated numbers', () => {
-        const stringCalculator = new StringCalculator;
         const result = stringCalculator.add('1,2')
 
         expect(result).toBe(3);
     })
 
     it('returns number if only one given', () => {
-        const stringCalculator = new StringCalculator;
         const result = stringCalculator.add('53')
 
         expect(result).toBe(53);
     })
 
     it('returns sum of double digit numbers', () => {
-        const stringCalculator = new StringCalculator;
         const result = stringCalculator.add('53,21')
 
         expect(result).toBe(74);
     })
 
     it('adds unknown amounts of numbers', () => {
-        const stringCalculator = new StringCalculator;
         const result = stringCalculator.add('53,2,5,4,2,8,4,9,14')
 
         expect(result).toBe(101);
