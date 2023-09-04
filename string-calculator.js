42b2a3f87448ee05ef900efc2338b516fd8444c3
MattW <mattnwarnock@yahoo.co.uk>
2023-09-04 17:06:03 +0100

    Add one or two numbers

--- string-calculator.js
+++ string-calculator.js
@@ -1,6 +1,17 @@
 class StringCalculator {
     add(string) {
-        return 0;
+        if(string.length < 1) {
+            return 0;
+        }
+        if(string.match(/\d,\d/)) {
+            const firstNumber = parseInt(string[0]);
+            const secondNumber = parseInt(string[2]);
+            return  firstNumber + secondNumber;
+        }
+
+        if(!string.match(/,/)) {
+            return parseInt(string);
+        }
     }
 }
 
--- string-calculator.test.js
+++ string-calculator.test.js
@@ -7,4 +7,25 @@ describe('String Calculator', () => {
 
         expect(result).toBe(0);
     })
+
+    it('returns sum of two comma separated numbers', () => {
+        const stringCalculator = new StringCalculator;
+        const result = stringCalculator.add('1,2')
+
+        expect(result).toBe(3);
+    })
+
+    it('returns number if only one given', () => {
+        const stringCalculator = new StringCalculator;
+        const result = stringCalculator.add('53')
+
+        expect(result).toBe(53);
+    })
+
+    xit('adds unknown amounts of numbers', () => {
+        const stringCalculator = new StringCalculator;
+        const result = stringCalculator.add('53,2,5,4,2,8,4,9,14')
+
+        expect(result).toBe(101);
+    })
 })
