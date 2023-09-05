aac659347ba0244c87397e23cd36e7eb9ae0fa57
MattW <mattnwarnock@yahoo.co.uk>
2023-09-05 14:47:17 +0100

    Refactor

--- string-calculator.js
+++ string-calculator.js
@@ -1,25 +1,37 @@
 class StringCalculator {
+  #separators = ",\n";
+  #customSeparatorPattern = /^[/]{2}.\n/;
+
   add(string) {
-    let separators = ",\n";
     let result = 0;
 
     if (string.length < 1) return result;
 
-    if (string.match(/^[/]{2}.\n/)) {
-      separators += string[2];
-      string = string.slice(4);
+    if (string.match(this.#customSeparatorPattern)) {
+      string = this._extractCustomSeparatorFrom(string);
     }
 
-    this._extractNumbers(string, separators).forEach((number) => {
+    this._extractNumbers(string).forEach((number) => {
       result += number;
     });
 
     return result;
   }
-  _extractNumbers(string, separators) {
-    const regex = new RegExp(`[${separators}]`, "i");
+
+  _extractCustomSeparatorFrom(string) {
+    this.#separators += string[2];
+
+    return string.slice(4);
+  }
+
+  _extractNumbers(string) {
+    const regex = this._generateRegex();
 
     return string.split(regex).map(Number);
+  }
+
+  _generateRegex() {
+    return new RegExp(`[${this.#separators}]`, "i");
   }
 }
 
