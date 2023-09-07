0afa12fef88bbc2f1ebe4485ed72cefb8adcdecb
MattW <mattnwarnock@yahoo.co.uk>
2023-09-07 14:04:41 +0100

    Refactor format tests

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -1,58 +1,77 @@
-const StringCalculator = require('./string-calculator');
+const StringCalculator = require("./string-calculator");
 
-describe('String Calculator', () => {
-    const stringCalculator = new StringCalculator;
+describe("String Calculator", () => {
+  const stringCalculator = new StringCalculator();
 
-    it('Returns zero when passed an empty string', () => {
-        const result = stringCalculator.add('');
+  it("Returns zero when passed an empty string", () => {
+    const result = stringCalculator.add("");
 
-        expect(result).toBe(0);
-    })
+    expect(result).toBe(0);
+  });
 
-    it('returns sum of two comma separated numbers', () => {
-        const result = stringCalculator.add('1,2')
+  it("returns sum of two comma separated numbers", () => {
+    const result = stringCalculator.add("1,2");
 
-        expect(result).toBe(3);
-    })
+    expect(result).toBe(3);
+  });
 
-    it('returns number if only one given', () => {
-        const result = stringCalculator.add('53')
+  it("returns number if only one given", () => {
+    const result = stringCalculator.add("53");
 
-        expect(result).toBe(53);
-    })
+    expect(result).toBe(53);
+  });
 
-    it('returns sum of double digit numbers', () => {
-        const result = stringCalculator.add('53,21')
+  it("returns sum of double digit numbers", () => {
+    const result = stringCalculator.add("53,21");
 
-        expect(result).toBe(74);
-    })
+    expect(result).toBe(74);
+  });
 
-    it('adds unknown amounts of numbers', () => {
-        const result = stringCalculator.add('53,2,5,4,2,8,4,9,14')
+  it("adds unknown amounts of numbers", () => {
+    const result = stringCalculator.add("53,2,5,4,2,8,4,9,14");
 
-        expect(result).toBe(101);
-    })
+    expect(result).toBe(101);
+  });
 
-    it('Returns sum of newline and comma separated numbers', () => {
-        const result = stringCalculator.add('3\n4,10')
+  it("Returns sum of newline and comma separated numbers", () => {
+    const result = stringCalculator.add("3\n4,10");
 
-        expect(result).toBe(17);
-    })
+    expect(result).toBe(17);
+  });
 
-    it('Separates numbers with custom separator', () => {
-        const result = stringCalculator.add('//;\n1;2')
+  it("Separates numbers with custom separator", () => {
+    const result = stringCalculator.add("//;\n1;2");
 
-        expect(result).toBe(3);
-    })
+    expect(result).toBe(3);
+  });
 
-    it('Throws an exception when passing a negative number with number in error message', () => {
-        const negativeNumber = -2;
+  describe("when passing any negative numbers", () => {
+    const negativeNumber = -2;
 
-        const result = () => {
-            stringCalculator.add(`1,${negativeNumber}`);
-        }
+    it("Throws an exception", () => {
+      expect(addWithNegative).toThrow(TypeError);
+    });
 
-        expect(result).toThrow(TypeError);
-        expect(result).toThrow(`error: negatives not allowed: ${negativeNumber}`);
-    })
-})
+    it("The exception describes the number in error", () => {
+      expect(addWithNegative).toThrow(
+        `error: negatives not allowed: ${negativeNumber}`
+      );
+    });
+
+    it("The exception describes multiple numbers in error", () => {
+      const secondNegativeNumber = -1;
+
+      const result = () => {
+        stringCalculator.add(`2,${negativeNumber},${secondNegativeNumber}`);
+      };
+
+      expect(result).toThrow(
+        `error: negatives not allowed: ${negativeNumber},${secondNegativeNumber}`
+      );
+    });
+
+    const addWithNegative = () => {
+      stringCalculator.add(`1,${negativeNumber}`);
+    };
+  });
+});
